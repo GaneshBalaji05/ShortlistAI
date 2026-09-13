c1bda37075cb5b1424e718741283bc71fde95266
GaneshBalaji05 <152364081+GaneshBalaji05@users.noreply.github.com>
2026-09-13 05:53:55 +0530

    Resolve dashboard branch service worker with latest mobile icon cache

--- static/sw.js
+++ static/sw.js
@@ -1,9 +1,8 @@
 const CACHE = 'shortlistai-v4';
 const APP_SHELL = [
   '/static/manifest.webmanifest',
-  '/static/icons/shortlistai-app-no-white.svg',
-  '/static/icons/icon-192.png',
-  '/static/icons/icon-512.png'
+  '/static/icons/icon-192-v2.png',
+  '/static/icons/icon-512-v2.png'
 ];
 
 self.addEventListener('install', event => {
